3c2bad10e1377fe0a50c61724d19b3757c8b71c9
Ansh Narsale <93337479+anshnarsale@users.noreply.github.com>
2026-09-19 16:28:50 +0530

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,4 @@
-// @codekit-prepend "/vendor/TweenLite.min.js";
-// @codekit-prepend "/vendor/TimelineLite.min.js";
-// @codekit-prepend "/vendor/CSSPlugin.min.js";
-// @codekit-prepend "/vendor/CSSRulePlugin.min.js";
-// @codekit-prepend "/vendor/ScrollToPlugin.min.js";
-// @codekit-prepend "/vendor/jquery.gsap.min.js";
-// @codekit-prepend "/vendor/ScrollMagic.min.js";
-// @codekit-prepend "/vendor/animation.gsap.min.js";
-// @codekit-prepend "/vendor/jquery.ScrollMagic.min.js";
+
 
 $(document).ready(function() {
 	
